c86698bc0a441a9510912a979a30cc7a50db94ae
Nikita Bardatskii <bardanik@fit.cvut.cz>
2025-04-07 11:30:44 +0200

    Refactor `Experiment` class to improve flexibility and tracking
    Updated the `Experiment` class to support dynamic parameter handling via builder methods, added experiment naming and random state initialization, and integrated MLflow for experiment tracking. Enhanced dataset and model logging preparation for better modularization.

--- workspace/sources/experiments/experiment.py
+++ workspace/sources/experiments/experiment.py
@@ -1,10 +1,17 @@
 from abc import ABC
-from datasets import Dataset
-from model import Model
+from datasets.dataset import Dataset
+from models.model import Model
+import mlflow
+from utils import generate_random_state
+
 class Experiment(ABC):
     class Builder:
         def __init__(self):
             self._params = {}
+
+        def with_name(self, name):
+            self._params["name"] = name
+            return self
 
         def with_dataset(self, dataset):
             self._params["dataset"] = dataset
@@ -14,16 +21,31 @@ class Experiment(ABC):
             self._params["model"] = model
             return self
 
+        def with_random_state(self, random_state):
+            self._params["random_state"] = random_state
+            return self
+
         def build(self):
-            experiment = Experiment()
-            for key, value in self._params.items():
-                setattr(experiment, key, value)
+            experiment = Experiment(**self._params)
             return experiment
 
-    def __init__(self):
-        self.dataset = None
-        self.model = None
+    def __init__(self, **kwargs):
+        self.dataset = kwargs['dataset']
+        self.model = kwargs['model']
+        self.name = kwargs.get('name', self.model.name)
+        self.random_state = kwargs.get('random_state', generate_random_state())
 
-    def conduct(self):
-        self.dataset.log()
-        self.model.log()
+    def __init_experiment(self):
+        self.mlflow_experiment = mlflow.set_experiment(self.name)
+        self.experiment_id = self.mlflow_experiment.experiment_id
+
+    def __prepare(self):
+
+        self.dataset.init_log()
+        self.model.init_log()
+
+    def run(self):
+        self.__init_experiment()
+        with mlflow.start_run(experiment_id=self.experiment_id) as experiment_run:
+            self.__prepare()
+            pass
