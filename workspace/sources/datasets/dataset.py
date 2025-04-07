90bd9875b1bff90b00edfab7b8e41c826f6cc368
Nikita Bardatskii <bardanik@fit.cvut.cz>
2025-04-07 11:30:29 +0200

    Refactor dataset initialization and logging.
    Renamed attributes for clarity and modularized Mlflow logging into a separate method. This enhances code readability and improves maintainability.

--- workspace/sources/datasets/dataset.py
+++ workspace/sources/datasets/dataset.py
@@ -2,15 +2,14 @@ import pandas as pd
 import mlflow
 from abc import ABC, abstractmethod
 class Dataset(ABC):
-    def __init__(self, dataset: pd.DataFrame):
-        self.dataset = dataset
-
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
 
 class ReCoveryDataset(Dataset):
-    def __init__(self, path='Recovery/recovery.csv'):
-        self.dataset_name = 'ReCovery'
-
-        mlflow.log_params('dataset_name', self.dataset_name)
-
+    def __init__(self, path):
+        self.name = 'ReCovery'
         dataset = pd.read_csv(path)
         super().__init__(dataset)
+
+    def init_log(self):
+        mlflow.log_param('dataset_name', self.name)
