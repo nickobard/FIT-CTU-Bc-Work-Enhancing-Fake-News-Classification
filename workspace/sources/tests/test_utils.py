cdd42f9172b01e8502ddbe90a6caebc0756d3b74
Nikita Bardatskii <bardanik@fit.cvut.cz>
2025-04-10 11:16:42 +0200

    Add `normalize_artifact_uri` function and test coverage
    Introduce `normalize_artifact_uri` to process artifact URIs into normalized local file paths. Added corresponding unit test to validate functionality and ensure proper pickle file operations, including creation, verification, and cleanup.

---
+++ workspace/sources/tests/test_utils.py
@@ -0,0 +1,42 @@
+import unittest
+from utils import normalize_artifact_uri
+
+
+class GeneralTestCase(unittest.TestCase):
+
+    def test_pickle_file_operations(self):
+        import pickle
+        import os
+
+        tests_path = "file:///" + os.path.normpath(os.path.join(os.getcwd(), "tests")).replace(
+            "\\", "/")
+        print(tests_path)
+
+        test_data = {"key": "value"}
+        test_file = os.path.normpath(os.path.join(normalize_artifact_uri(tests_path), "dummy_test_file.pkl"))
+
+        # Check the file does not exist
+        self.assertFalse(os.path.exists(test_file), "Test file should not exist initially.")
+
+
+        # Save dummy pickle file
+        with open(test_file, "wb") as f:
+            pickle.dump(test_data, f)
+
+        # Check the file exists
+        self.assertTrue(os.path.exists(test_file), "Test file should exist after being created.")
+
+        # Load and verify contents
+        with open(test_file, "rb") as f:
+            loaded_data = pickle.load(f)
+        self.assertEqual(loaded_data, test_data)
+
+        # Remove the file
+        os.remove(test_file)
+
+        # Confirm file no longer exists
+        self.assertFalse(os.path.exists(test_file), "Test file should no longer exist after deletion.")
+
+
+if __name__ == '__main__':
+    unittest.main()
--- workspace/sources/utils.py
+++ workspace/sources/utils.py
@@ -1,4 +1,15 @@
 import os
+import pickle
+from urllib.parse import urlparse
+from urllib.request import url2pathname
+
 
 def generate_random_state():
     return int.from_bytes(os.urandom(4), byteorder="big")
+
+
+def normalize_artifact_uri(artifact_uri: str) -> str:
+    parsed_path = urlparse(artifact_uri).path
+    local_path = url2pathname(parsed_path)
+    normalized_path = os.path.normpath(local_path)
+    return normalized_path
