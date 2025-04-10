bbf0129529964b85b8319d3bc1b8d57998f5339f
Nikita Bardatskii <bardanik@fit.cvut.cz>
2025-04-10 11:27:19 +0200

    Rename utility function for clarity and consistency
    Renamed `normalize_artifact_uri` to `get_normalized_path_from_artifact_uri` for improved readability and to better reflect its purpose. Removed unused `pickle` import to clean up the codebase.

--- workspace/sources/utils.py
+++ workspace/sources/utils.py
@@ -1,5 +1,4 @@
 import os
-import pickle
 from urllib.parse import urlparse
 from urllib.request import url2pathname
 
@@ -8,7 +7,7 @@ def generate_random_state():
     return int.from_bytes(os.urandom(4), byteorder="big")
 
 
-def normalize_artifact_uri(artifact_uri: str) -> str:
+def get_normalized_path_from_artifact_uri(artifact_uri: str) -> str:
     parsed_path = urlparse(artifact_uri).path
     local_path = url2pathname(parsed_path)
     normalized_path = os.path.normpath(local_path)
