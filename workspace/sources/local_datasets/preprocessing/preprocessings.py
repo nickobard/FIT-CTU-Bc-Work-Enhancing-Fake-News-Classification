d9ef89727e46626791559991f21e2208ff41b2fe
Nikita Bardatskii <bardanik@fit.cvut.cz>
2025-05-07 14:41:29 +0200

    Refactor preprocessing classes for clarity and flexibility
    Replaced initialization-based preprocessing logic with method-based implementations to streamline data transformations. Introduced new preprocessing classes for URL removal, stemming, and lemmatization with language-specific support. This improves modularity and extends functionality for text preprocessing.

--- workspace/sources/local_datasets/preprocessing/preprocessings.py
+++ workspace/sources/local_datasets/preprocessing/preprocessings.py
@@ -1,20 +1,70 @@
 from .base import Preprocessing
-from .utils import remove_punctation, remove_numbers, remove_special_characters
+from .flajzik_preprocessings import remove_urls, remove_punctation, remove_numbers, remove_special_characters, stem, \
+    lemmatize
+from typing import Literal
 
 
 class PunctuationRemoval(Preprocessing):
-    def __init__(self):
-        super().__init__()
-        self.preprocessing_function = remove_punctation
+    def preprocess(self, data):
+        data.features = remove_punctation(data.features)
+        return data
 
 
 class NumbersRemoval(Preprocessing):
-    def __init__(self):
-        super().__init__()
-        self.preprocessing_function = remove_numbers
+    def preprocess(self, data):
+        data.features = remove_numbers(data.features)
+        return data
+
+
+class URLsRemoval(Preprocessing):
+    def preprocess(self, data):
+        data.features = remove_urls(data.features)
+        return data
 
 
 class SpecialCharactersRemoval(Preprocessing):
-    def __init__(self):
+    def preprocess(self, data):
+        data.features = remove_special_characters(data.features)
+        return data
+
+
+class NoiseReduction(SpecialCharactersRemoval):
+    pass
+
+
+class Stemming(Preprocessing):
+    def __init__(self, language: Literal['english', 'czech'] = 'english'):
         super().__init__()
-        self.preprocessing_function = remove_special_characters
+        self.language = language
+        self.language_codes = {'english': 'en',
+                               'czech': 'cs'}
+
+    @property
+    def _language_code(self):
+        return self.language_codes[self.language]
+
+    def hyperparameters_output(self):
+        return {'stemmer': self.language}
+
+    def preprocess(self, data):
+        data.features = stem(data.features, self._language_code)
+        return data
+
+
+class Lemmatization(Preprocessing):
+    def __init__(self, language: Literal['english', 'czech'] = 'english'):
+        super().__init__()
+        self.language = language
+        self.language_codes = {'english': 'en',
+                               'czech': 'cs'}
+
+    def hyperparameters_output(self):
+        return {'lemmatizer': self.language}
+
+    @property
+    def _language_code(self):
+        return self.language_codes[self.language]
+
+    def preprocess(self, data):
+        data.features = lemmatize(data.features, self._language_code)
+        return data
