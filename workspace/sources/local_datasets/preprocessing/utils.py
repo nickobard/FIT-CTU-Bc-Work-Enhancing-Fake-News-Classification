16b19366c26762cd84538e3f0fb69e7d64910c9e
Nikita Bardatskii <bardanik@fit.cvut.cz>
2025-05-07 18:47:00 +0200

    Add more preprocessings pipelines

--- workspace/sources/local_datasets/preprocessing/utils.py
+++ workspace/sources/local_datasets/preprocessing/utils.py
@@ -1,4 +1,10 @@
-from local_datasets.preprocessing.tokenization import BertBasedUncasedTokenizer
+from .tokenization import BertBaseUncasedTokenizer, NLTKTokenizer
+from .encoding import BertBaseUncasedEncoder
+from .preprocessings import NoiseReduction, Stemming, Lemmatization
 
-standard_bert_pipeline = [BertBasedUncasedTokenizer()]
-
+minimal_bert_pipeline = [BertBaseUncasedTokenizer(), BertBaseUncasedEncoder()]
+cleaned_bert_pipeline = [NoiseReduction(), BertBaseUncasedTokenizer(), BertBaseUncasedEncoder()]
+cleaned_stemmed_bert_pipeline = [NoiseReduction(), NLTKTokenizer(), Stemming(), BertBaseUncasedTokenizer(),
+                                 BertBaseUncasedEncoder()]
+cleaned_lemmatized_bert_pipeline = [NoiseReduction(), NLTKTokenizer(), Lemmatization(), BertBaseUncasedTokenizer(),
+                                    BertBaseUncasedEncoder()]
