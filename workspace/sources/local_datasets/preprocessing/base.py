6681558a4bf8e6fa9eeb470c8ddee3d72a31815e
Nikita Bardatskii <bardanik@fit.cvut.cz>
2025-05-08 17:56:42 +0200

    Refactor preprocessing base to enhance data handling.
    Introduce utility methods for checking, loading, and saving preprocessed data. Add logging support and improve class structure for better extensibility and clarity. Replace `OUTPUT_DATA_CLASS` with `PREPROCESSED_DATA_CLASS` for clearer semantics.

--- workspace/sources/local_datasets/preprocessing/base.py
+++ workspace/sources/local_datasets/preprocessing/base.py
@@ -1,16 +1,25 @@
 import os
 import re
 from abc import ABC, abstractmethod
-import datasets as hf_datasets
+from logging import getLogger
+from pathlib import Path
 
-from ..data_classes import HuggingFaceData, PandasData
+from ..data_classes import PandasData
 
 
 class Preprocessing(ABC):
-    OUTPUT_DATA_CLASS = PandasData
+    PREPROCESSED_DATA_CLASS = PandasData
+
+    def __init__(self):
+        self.logger = None
+
     @abstractmethod
     def preprocess(self, data):
         pass
+
+    def init(self, logger=None):
+        self.logger = logger if logger else getLogger()
+        return self
 
     def hyperparameters_output(self):
         return {}
@@ -19,11 +28,30 @@ class Preprocessing(ABC):
         name = self.__class__.__name__
         return re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
 
+    def preprocessed_data_exists(self, artifacts_path, split_name=None, name_dir_prefix=''):
+        if artifacts_path is None:
+            return False
+        if name_dir_prefix:
+            name_dir = '_'.join([name_dir_prefix, self.name()])
+        else:
+            name_dir = self.name()
+        path = os.path.join(artifacts_path, name_dir, split_name)
+        return self.PREPROCESSED_DATA_CLASS.saved_data_exists(path)
 
-class HuggingFaceDatasetConversion(Preprocessing):
-    OUTPUT_DATA_CLASS = HuggingFaceData
+    def load(self, artifacts_path, split_name, name_dir_prefix=''):
+        if name_dir_prefix:
+            name_dir = '_'.join([name_dir_prefix, self.name()])
+        else:
+            name_dir = self.name()
+        path = os.path.join(artifacts_path, name_dir, split_name)
+        data = self.PREPROCESSED_DATA_CLASS.load(path)
+        return data
 
-    def preprocess(self, data):
-        hf_data = hf_datasets.Dataset.from_pandas(data.dataset)
-        hf_data.set_format(type='torch')
-        return HuggingFaceData(hf_data)
+    def save_preprocessed_data(self, artifacts_path, split_name, data, name_dir_prefix=''):
+        if name_dir_prefix:
+            name_dir = '_'.join([name_dir_prefix, self.name()])
+        else:
+            name_dir = self.name()
+        path = os.path.join(artifacts_path, name_dir, split_name)
+        Path(path).parent.mkdir(parents=True, exist_ok=True)
+        data.save(path)
