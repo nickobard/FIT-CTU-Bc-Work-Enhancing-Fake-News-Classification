62cc63a7085e9c7e0287f37fe8ce0be5ed9d7ee5
Nikita Bardatskii <bardanik@fit.cvut.cz>
2025-05-08 13:48:27 +0200

    Add `name` method and `OUTPUT_DATA_CLASS` attribute
    Introduce a `name` method to dynamically generate class names in snake_case using regex. Define `OUTPUT_DATA_CLASS` as a class attribute for better output type specification and ensure compatibility across preprocessing subclasses.

--- workspace/sources/local_datasets/preprocessing/base.py
+++ workspace/sources/local_datasets/preprocessing/base.py
@@ -1,11 +1,13 @@
+import os
+import re
 from abc import ABC, abstractmethod
 import datasets as hf_datasets
 
-from local_datasets.dataset import HuggingFaceData
+from ..data_classes import HuggingFaceData, PandasData
 
 
 class Preprocessing(ABC):
-
+    OUTPUT_DATA_CLASS = PandasData
     @abstractmethod
     def preprocess(self, data):
         pass
@@ -13,8 +15,14 @@ class Preprocessing(ABC):
     def hyperparameters_output(self):
         return {}
 
+    def name(self):
+        name = self.__class__.__name__
+        return re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+
 
 class HuggingFaceDatasetConversion(Preprocessing):
+    OUTPUT_DATA_CLASS = HuggingFaceData
+
     def preprocess(self, data):
         hf_data = hf_datasets.Dataset.from_pandas(data.dataset)
         hf_data.set_format(type='torch')
