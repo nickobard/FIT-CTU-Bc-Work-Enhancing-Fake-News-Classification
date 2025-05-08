b7719987eabbf40953b54be1490b6ebf05deed3d
Nikita Bardatskii <bardanik@fit.cvut.cz>
2025-05-08 17:57:10 +0200

    Add support for saving and loading data objects using pickle
    Introduced methods to save data objects to both CSV and pickle formats, ensuring efficient persistence and restoration. Refactored the `PandasData` class to separate features and labels for better flexibility and consistency. Updated related functions to align with the new data handling approach.

--- workspace/sources/local_datasets/data_classes.py
+++ workspace/sources/local_datasets/data_classes.py
@@ -1,3 +1,4 @@
+import pickle
 from abc import abstractmethod
 import os
 from pathlib import Path
@@ -6,46 +7,51 @@ import datasets as hf_datasets
 
 
 class Data:
+
     @abstractmethod
     def to_csv(self, path):
         pass
 
+    def to_pickle(self, path):
+        pickle_path = path + '_data_obj.pkl'
+        with open(pickle_path, 'wb') as f:
+            pickle.dump(self, f)
+        return self
 
-class PandasData(Data):
-    def __init__(self, dataset, features_column_name='article', labels_column_name='label'):
-        self.features_colname = features_column_name
-        self.labels_colname = labels_column_name
-        self.dataset = dataset
+    def save(self, path):
+        file_path = Path(path)
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        self.to_pickle(path)
+        self.to_csv(path)
+        return self
 
-    @property
-    def features(self):
-        return self.dataset[self.features_colname]
-
-    @features.setter
-    def features(self, value):
-        self.dataset[self.features_colname] = value
-
-    @property
-    def labels(self):
-        return self.dataset[self.labels_colname]
-
-    @labels.setter
-    def labels(self, value):
-        self.dataset[self.labels_colname] = value
-
-    def copy(self):
-        return self.__class__(self.dataset.copy(),
-                              self.features_colname,
-                              self.labels_colname)
+    @classmethod
+    def saved_data_exists(cls, path):
+        pickle_file_exists = os.path.exists(path + 'data_obj.pkl')
+        csv_file_exists = os.path.exists(path + '.csv')
+        return pickle_file_exists and csv_file_exists
 
     @classmethod
     def load(cls, path):
-        dataset = pd.read_csv(path)
-        return cls(dataset)
+        with open(path + 'data_obj.pkl', 'rb') as f:
+            data = pickle.load(f)
+        return data
+
+
+class PandasData(Data):
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
+
+    @property
+    def dataset(self):
+        return pd.concat([self.features, self.labels], axis=1)
+
+    def copy(self):
+        return self.__class__(self.features.copy(), self.labels.copy())
 
     def to_csv(self, path):
-        Path(os.path.dirname(path)).mkdir(parents=True, exist_ok=True)
-        self.dataset.to_csv(path, index=False)
+        self.dataset.to_csv(path + '.csv', index=False)
 
     def is_empty(self):
         return len(self.dataset) == 0
@@ -58,8 +64,7 @@ class HuggingFaceData(Data):
         self.dataset = dataset
 
     def to_csv(self, path):
-        Path(os.path.dirname(path)).mkdir(parents=True, exist_ok=True)
-        self.dataset.to_csv(path, index=False)
+        self.dataset.to_csv(path + '.csv', index=False)
 
     @classmethod
     def load(cls, path):
