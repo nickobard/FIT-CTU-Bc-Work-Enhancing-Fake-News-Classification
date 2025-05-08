e2f40565e1cbeaea75b3c1ea8e9b15b1e2ad6f1e
Nikita Bardatskii <bardanik@fit.cvut.cz>
2025-05-08 13:39:17 +0200

    Add HuggingFaceDatasetConversion to preprocessing pipelines
    Refactored the minimal, cleaned, cleaned-stemmed, and cleaned-lemmatized BERT pipelines to include HuggingFaceDatasetConversion for consistent dataset conversion functionality. Adjusted module imports to reflect cleaner organization.

--- workspace/sources/local_datasets/preprocessing/utils.py
+++ workspace/sources/local_datasets/preprocessing/utils.py
@@ -1,10 +1,27 @@
+from .base import HuggingFaceDatasetConversion
 from .tokenization import BertBaseUncasedTokenizer, NLTKTokenizer
 from .encoding import BertBaseUncasedEncoder
-from .preprocessings import NoiseReduction, Stemming, Lemmatization
+from .cleaning import NoiseReduction, Stemming, Lemmatization
 
-minimal_bert_pipeline = [BertBaseUncasedTokenizer(), BertBaseUncasedEncoder()]
-cleaned_bert_pipeline = [NoiseReduction(), BertBaseUncasedTokenizer(), BertBaseUncasedEncoder()]
-cleaned_stemmed_bert_pipeline = [NoiseReduction(), NLTKTokenizer(), Stemming(), BertBaseUncasedTokenizer(),
-                                 BertBaseUncasedEncoder()]
-cleaned_lemmatized_bert_pipeline = [NoiseReduction(), NLTKTokenizer(), Lemmatization(), BertBaseUncasedTokenizer(),
-                                    BertBaseUncasedEncoder()]
+minimal_bert_pipeline = [BertBaseUncasedTokenizer(),
+                         BertBaseUncasedEncoder(),
+                         HuggingFaceDatasetConversion()]
+
+cleaned_bert_pipeline = [NoiseReduction(),
+                         BertBaseUncasedTokenizer(),
+                         BertBaseUncasedEncoder(),
+                         HuggingFaceDatasetConversion()]
+
+cleaned_stemmed_bert_pipeline = [NoiseReduction(),
+                                 NLTKTokenizer(),
+                                 Stemming(),
+                                 BertBaseUncasedTokenizer(),
+                                 BertBaseUncasedEncoder(),
+                                 HuggingFaceDatasetConversion()]
+
+cleaned_lemmatized_bert_pipeline = [NoiseReduction(),
+                                    NLTKTokenizer(),
+                                    Lemmatization(),
+                                    BertBaseUncasedTokenizer(),
+                                    BertBaseUncasedEncoder(),
+                                    HuggingFaceDatasetConversion()]
