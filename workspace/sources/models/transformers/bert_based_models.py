26f6eb987f35e29f05ff649112f94b9cb1265917
Nikita Bardatskii <bardanik@fit.cvut.cz>
2025-05-15 21:47:31 +0200

    Refactor transformer models to support training/evaluation args
    Refactored BERT, RoBERTa, DistilliBERT, GPT1, and GPT2 models to include `training_arguments`, `train_best_model_metric`, and `evaluation_metrics` in their constructors. This enhances flexibility by enabling specification of training configurations and metric evaluation.

--- workspace/sources/models/transformers/bert_based_models.py
+++ workspace/sources/models/transformers/bert_based_models.py
@@ -1,22 +1,45 @@
 from .base import Transformer
+from ...experiments.metrics import Loss
+from experiments.metrics import standard_evaluation_metrics
 
 
 class BERT(Transformer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = 'BERT'
-        self.transformers_identifier = 'bert-base-uncased'
+    def __init__(self,
+                 name='BERT',
+                 transformers_identifier='bert-base-uncased',
+                 training_arguments=None,
+                 train_best_model_metric=Loss,
+                 evaluation_metrics=standard_evaluation_metrics):
+        super().__init__(name,
+                         transformers_identifier,
+                         training_arguments,
+                         train_best_model_metric,
+                         evaluation_metrics)
 
 
 class DistilliBERT(Transformer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = 'DistilliBERT'
-        self.transformers_identifier = 'distilbert-base-uncased'
+    def __init__(self,
+                 name='DistilliBERT',
+                 transformers_identifier='distilbert-base-uncased',
+                 training_arguments=None,
+                 train_best_model_metric=Loss,
+                 evaluation_metrics=standard_evaluation_metrics):
+        super().__init__(name,
+                         transformers_identifier,
+                         training_arguments,
+                         train_best_model_metric,
+                         evaluation_metrics)
 
 
 class RoBERTa(Transformer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = 'RoBERTa'
-        self.transformers_identifier = 'roberta-base'
+    def __init__(self,
+                 name='RoBERTa',
+                 transformers_identifier='roberta-base',
+                 training_arguments=None,
+                 train_best_model_metric=Loss,
+                 evaluation_metrics=standard_evaluation_metrics):
+        super().__init__(name,
+                         transformers_identifier,
+                         training_arguments,
+                         train_best_model_metric,
+                         evaluation_metrics)
--- workspace/sources/models/transformers/openai_gpt_models.py
+++ workspace/sources/models/transformers/openai_gpt_models.py
@@ -1,12 +1,26 @@
 from transformers import AutoModelForSequenceClassification
+from ...experiments.metrics import Loss
+from experiments.metrics import standard_evaluation_metrics
 
 from .base import Transformer
 
 
 class OpenAI_GPT(Transformer):
 
-    def __init__(self, gpt_encoder, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self,
+                 name,
+                 transformers_identifier,
+                 gpt_encoder,
+                 training_arguments=None,
+                 train_best_model_metric=Loss,
+                 evaluation_metrics=standard_evaluation_metrics
+
+                 ):
+        super().__init__(name,
+                         transformers_identifier,
+                         training_arguments,
+                         train_best_model_metric,
+                         evaluation_metrics)
         self.encoder = gpt_encoder
 
     def _init_model(self):
@@ -24,14 +38,32 @@ class OpenAI_GPT(Transformer):
 
 
 class GPT1(OpenAI_GPT):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = 'GPT1'
-        self.transformers_identifier = 'openai-gpt'
+    def __init__(self,
+                 gpt_encoder,
+                 name='GPT1',
+                 transformers_identifier='openai-gpt',
+                 training_arguments=None,
+                 train_best_model_metric=Loss,
+                 evaluation_metrics=standard_evaluation_metrics):
+        super().__init__(name,
+                         transformers_identifier,
+                         gpt_encoder,
+                         training_arguments,
+                         train_best_model_metric,
+                         evaluation_metrics)
 
 
 class GPT2(OpenAI_GPT):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = 'GPT2'
-        self.transformers_identifier = 'gpt2'
+    def __init__(self,
+                 gpt_encoder,
+                 name='GPT2',
+                 transformers_identifier='gpt2',
+                 training_arguments=None,
+                 train_best_model_metric=Loss,
+                 evaluation_metrics=standard_evaluation_metrics):
+        super().__init__(name,
+                         transformers_identifier,
+                         gpt_encoder,
+                         training_arguments,
+                         train_best_model_metric,
+                         evaluation_metrics)
